2ac9bf098f812b327b8b47f5be44b1c1df12a58f
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2024-12-20 23:00:04 +0000

    stub: database interface stub

---
+++ pkg/database/database.go
@@ -0,0 +1,15 @@
+package database
+
+import "fmt"
+
+type Database interface {
+	InsertCheck(host, port, protocol, status string, elapsed int64) error
+	Close() error
+}
+
+func NewDatabase(dbType, dbPath string) (Database, error) {
+	switch dbType {
+	default:
+		return nil, fmt.Errorf("unsupported database type: %s", dbType)
+	}
+}
