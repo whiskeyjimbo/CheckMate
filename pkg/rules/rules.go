4b3ede5c02874d8c1ca2200d9c86ad097ff1e6dc
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2024-12-25 06:31:32 +0000

    refactor: remove double wrapping of errors

--- pkg/rules/rules.go
+++ pkg/rules/rules.go
@@ -1,12 +1,12 @@
 package rules
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/expr-lang/expr"
-	"errors"
 )
 
 type Rule struct {
@@ -17,8 +17,8 @@ type Rule struct {
 var (
 	ErrConditionProcessing = errors.New("failed to process condition")
 	ErrConditionCompile    = errors.New("failed to compile rule condition")
-	ErrConditionEval      = errors.New("failed to evaluate rule condition")
-	ErrNotBoolean         = errors.New("rule condition did not evaluate to a boolean")
+	ErrConditionEval       = errors.New("failed to evaluate rule condition")
+	ErrNotBoolean          = errors.New("rule condition did not evaluate to a boolean")
 )
 
 type RuleEnvironment struct {
@@ -29,21 +29,23 @@ type RuleEnvironment struct {
 // Main function to evaluate a rule
 func EvaluateRule(rule Rule, downtime time.Duration, responseTime time.Duration) (bool, error) {
 	env := createExprEnv(downtime, responseTime)
+
 	program, err := expr.Compile(rule.Condition, expr.Env(env))
 	if err != nil {
-		processedCondition, err := processCondition(rule.Condition, downtime, responseTime)
-		if err != nil {
-			return false, fmt.Errorf("%w: %v", ErrConditionProcessing, err)
+		processedCondition, procErr := processCondition(rule.Condition, downtime, responseTime)
+		if procErr != nil {
+			return false, procErr // No need to wrap as processCondition adds context
 		}
+
 		program, err = expr.Compile(processedCondition, expr.Env(env))
 		if err != nil {
-			return false, fmt.Errorf("%w: %v", ErrConditionCompile, err)
+			return false, fmt.Errorf("rule '%s': %w: %v", rule.Name, ErrConditionCompile, err)
 		}
 	}
 
 	output, err := expr.Run(program, env)
 	if err != nil {
-		return false, fmt.Errorf("%w: %v", ErrConditionEval, err)
+		return false, fmt.Errorf("rule '%s': %w: %v", rule.Name, ErrConditionEval, err)
 	}
 
 	result, ok := output.(bool)
@@ -64,7 +66,7 @@ func createExprEnv(downtime, responseTime time.Duration) RuleEnvironment {
 func processCondition(condition string, downtime, responseTime time.Duration) (string, error) {
 	condition = strings.ReplaceAll(condition, "${downtime}", fmt.Sprintf("%d", timeDurationToSeconds(downtime)))
 	condition = strings.ReplaceAll(condition, "${responseTime}", fmt.Sprintf("%d", timeDurationToSeconds(responseTime)))
-	
+
 	words := strings.Split(condition, " ")
 	for i, word := range words {
 		dur, err := time.ParseDuration(word)
