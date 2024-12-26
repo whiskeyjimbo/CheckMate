c00056e39bd9e10cd032c9e3a845ca08451e1da2
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2024-12-26 19:00:18 +0000

    feat: update notifications interface with structured Notification type and other stuff

--- pkg/notifications/notifier.go
+++ pkg/notifications/notifier.go
@@ -1,6 +1,35 @@
 package notifications
 
-// stub out notifications interface, will probably need more params
+import "context"
+
+type NotificationType string
+
+const (
+	LogNotification NotificationType = "log"
+	// SlackNotification NotificationType = "slack"
+	// EmailNotification NotificationType = "email"
+)
+
+type NotificationLevel string
+
+const (
+	InfoLevel    NotificationLevel = "info"
+	WarningLevel NotificationLevel = "warning"
+	ErrorLevel   NotificationLevel = "error"
+)
+
+type Notification struct {
+	Message  string
+	Level    NotificationLevel
+	Tags     []string
+	Host     string
+	Port     string
+	Protocol string
+}
+
 type Notifier interface {
-	SendNotification(message string) error
+	SendNotification(ctx context.Context, notification Notification) error
+	Type() NotificationType
+	Initialize(ctx context.Context) error
+	Close() error
 }
