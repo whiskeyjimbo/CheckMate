232eb52ee38a83b422c5119dac7c08658a7262db
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2024-12-26 19:02:58 +0000

    feat: add Log Notification implementation base for testing

---
+++ pkg/notifications/log_notifier.go
@@ -0,0 +1,42 @@
+package notifications
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+// Stub out log notifier interface
+type LogNotifier struct {
+	logger *zap.SugaredLogger
+}
+
+func NewLogNotifier(logger *zap.SugaredLogger) *LogNotifier {
+	return &LogNotifier{
+		logger: logger,
+	}
+}
+
+func (n *LogNotifier) SendNotification(ctx context.Context, notification Notification) error {
+	n.logger.With(
+		"level", notification.Level,
+		"host", notification.Host,
+		"port", notification.Port,
+		"protocol", notification.Protocol,
+		"tags", notification.Tags,
+	).Info(notification.Message)
+
+	return nil
+}
+
+func (n *LogNotifier) Type() NotificationType {
+	return LogNotification
+}
+
+func (n *LogNotifier) Initialize(ctx context.Context) error {
+	return nil
+}
+
+func (n *LogNotifier) Close() error {
+	return nil
+}
