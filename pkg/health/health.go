446c5af403254dcbf39572656cbec09eaf16b730
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2024-12-26 18:33:12 +0000

    feat: implement readiness check

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -3,6 +3,7 @@ package health
 import (
 	"encoding/json"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
@@ -10,6 +11,8 @@ type Response struct {
 	Status    string    `json:"status"`
 	Timestamp time.Time `json:"timestamp"`
 }
+
+var isReady atomic.Bool
 
 func LivenessHandler(w http.ResponseWriter, r *http.Request) {
 	response := Response{
@@ -22,11 +25,23 @@ func LivenessHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	response := Response{
-		Status:    "READY",
 		Timestamp: time.Now(),
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	if isReady.Load() {
+		response.Status = "READY"
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	response.Status = "NOT_READY"
+	w.WriteHeader(http.StatusServiceUnavailable)
 	json.NewEncoder(w).Encode(response)
+}
+
+func SetReady(ready bool) {
+	isReady.Store(ready)
 }
