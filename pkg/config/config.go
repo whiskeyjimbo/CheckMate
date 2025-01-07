415ab95c0871d89ba18616bd2724c6ddf72ca596
whiskeyjimbo <15094606+whiskeyjimbo@users.noreply.github.com>
2025-01-07 07:53:37 +0000

    refactor: restructure configuration types and  add some validation logic

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,31 +10,25 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type CheckConfig struct {
-	Port     string   `yaml:"port"`
-	Protocol string   `yaml:"protocol"`
-	Interval string   `yaml:"interval"`
-	Tags     []string `yaml:"tags"`
-	RuleMode RuleMode `yaml:"ruleMode,omitempty"`
-}
-
-type HostConfig struct {
-	Host     string        `yaml:"host"`
-	Tags     []string      `yaml:"tags"`
-	Checks   []CheckConfig `yaml:"checks"`
-	RuleMode RuleMode      `yaml:"ruleMode,omitempty"`
-}
-
-type NotificationConfig struct {
-	Type string `yaml:"type"`
-}
-
 type RuleMode string
 
 const (
 	RuleModeAll RuleMode = "all" // Fire rules only if all hosts are down (default)
 	RuleModeAny RuleMode = "any" // Fire rules if any host is down
 )
+
+type Config struct {
+	MonitorSite   string               `yaml:"monitorSite"`
+	Sites         []SiteConfig         `yaml:"sites"`
+	Rules         []rules.Rule         `yaml:"rules"`
+	Notifications []NotificationConfig `yaml:"notifications"`
+}
+
+type SiteConfig struct {
+	Name   string        `yaml:"name"`
+	Tags   []string      `yaml:"tags"`
+	Groups []GroupConfig `yaml:"groups"`
+}
 
 type GroupConfig struct {
 	Name     string        `yaml:"name"`
@@ -44,74 +38,106 @@ type GroupConfig struct {
 	RuleMode RuleMode      `yaml:"ruleMode,omitempty"`
 }
 
-func (g *GroupConfig) Validate() error {
-	if g.RuleMode == "" {
-		g.RuleMode = RuleModeAll
-	}
-
-	if g.RuleMode != RuleModeAll && g.RuleMode != RuleModeAny {
-		return fmt.Errorf("invalid rule mode: %s", g.RuleMode)
-	}
-	return nil
+type HostConfig struct {
+	Host     string        `yaml:"host"`
+	Tags     []string      `yaml:"tags"`
+	Checks   []CheckConfig `yaml:"checks"`
+	RuleMode RuleMode      `yaml:"ruleMode,omitempty"`
 }
 
-type SiteConfig struct {
-	Name   string        `yaml:"name"`
-	Tags   []string      `yaml:"tags"`
-	Groups []GroupConfig `yaml:"groups"`
+type CheckConfig struct {
+	Port     string   `yaml:"port"`
+	Protocol string   `yaml:"protocol"`
+	Interval string   `yaml:"interval"`
+	Tags     []string `yaml:"tags"`
+	RuleMode RuleMode `yaml:"ruleMode,omitempty"`
 }
 
-type Config struct {
-	MonitorSite   string               `yaml:"monitorSite"`
-	Sites         []SiteConfig         `yaml:"sites"`
-	Rules         []rules.Rule         `yaml:"rules"`
-	Notifications []NotificationConfig `yaml:"notifications"`
-}
-
-func loadConfig(configFile string) (*Config, error) {
-	fileContent, err := os.ReadFile(configFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read config file: %w", err)
-	}
-
-	var config Config
-	if err := yaml.Unmarshal(fileContent, &config); err != nil {
-		return nil, fmt.Errorf("failed to parse YAML: %w", err)
-	}
-
-	if config.MonitorSite == "" {
-		config.MonitorSite = "monitor-site"
-	}
-
-	for _, site := range config.Sites {
-		for _, host := range site.Groups {
-			for i := range host.Checks {
-				normalizeConfig(&host.Checks[i])
-			}
-		}
-	}
-
-	return &config, nil
+type NotificationConfig struct {
+	Type string `yaml:"type"`
 }
 
 func LoadConfiguration(args []string) (*Config, error) {
 	configFile := "config.yaml"
-	if len(os.Args) > 1 {
-		configFile = os.Args[1]
+	if len(args) > 1 {
+		configFile = args[1]
 	}
 
 	config, err := loadConfig(configFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load config: %w", err)
 	}
+
+	if err := validateConfig(config); err != nil {
+		return nil, fmt.Errorf("invalid configuration: %w", err)
+	}
+
+	normalizeConfig(config)
 	return config, nil
 }
 
-func normalizeConfig(c *CheckConfig) {
+func loadConfig(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func validateConfig(c *Config) error {
+	if c.MonitorSite == "" {
+		return fmt.Errorf("monitorSite must be specified")
+	}
+
+	for _, site := range c.Sites {
+		if site.Name == "" {
+			return fmt.Errorf("site name cannot be empty")
+		}
+		for _, group := range site.Groups {
+			if err := group.Validate(); err != nil {
+				return fmt.Errorf("invalid group '%s': %w", group.Name, err)
+			}
+		}
+	}
+	return nil
+}
+
+func normalizeConfig(c *Config) {
+	for i := range c.Sites {
+		for j := range c.Sites[i].Groups {
+			for k := range c.Sites[i].Groups[j].Checks {
+				normalizeCheck(&c.Sites[i].Groups[j].Checks[k])
+			}
+			for k := range c.Sites[i].Groups[j].Hosts {
+				for l := range c.Sites[i].Groups[j].Hosts[k].Checks {
+					normalizeCheck(&c.Sites[i].Groups[j].Hosts[k].Checks[l])
+				}
+			}
+		}
+	}
+}
+
+func normalizeCheck(c *CheckConfig) {
 	c.Protocol = strings.ToUpper(c.Protocol)
 	if _, err := strconv.Atoi(c.Interval); err == nil {
 		c.Interval = c.Interval + "s"
 	}
+}
+
+func (g *GroupConfig) Validate() error {
+	if g.Name == "" {
+		return fmt.Errorf("group name cannot be empty")
+	}
+	if g.RuleMode == "" {
+		g.RuleMode = RuleModeAll
+	}
+	return nil
 }
 
 type RuleModeResolver struct {
@@ -128,11 +154,9 @@ func (r *RuleModeResolver) GetEffectiveRuleMode(check CheckConfig) RuleMode {
 	if check.RuleMode != "" {
 		return check.RuleMode
 	}
-
 	if r.Group.RuleMode != "" {
 		return r.Group.RuleMode
 	}
-
 	return RuleModeAll
 }
 
